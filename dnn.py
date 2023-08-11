050bf2d8f932a72a466c19e1a3fa5535c18583f0
Ehsan Poursaeed <hsnprsd@gmail.com>
2023-08-11 17:53:18 +0330

    remove -> None

--- dnn.py
+++ dnn.py
@@ -3,7 +3,7 @@ import torch.nn.functional as F
 
 
 class MNIST(nn.Module):
-    def __init__(self, input_size, hidden_size, num_classes) -> None:
+    def __init__(self, input_size, hidden_size, num_classes):
         super().__init__()
 
         self.nn = nn.Sequential(
